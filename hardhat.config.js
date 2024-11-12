e8e7a7ed67dc8fc6623de3c4b1787305c40eb339
nsc <nsc@qq.com>
2024-11-12 09:19:49 +0800

    Adding Foundry to a Hardhat project

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-contract-sizer")
 require('@typechain/hardhat')
+require("@nomicfoundation/hardhat-foundry");
 
 const {
   BSC_URL,
